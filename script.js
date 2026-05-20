27b8aff432898a42a7ed3ea761a5ead4a241279d
jaydenc390 <jaydenc390@nycstudents.net>
2026-05-20 12:41:02 -0400

    done

---
+++ script.js
@@ -0,0 +1,30 @@
+let firstName = document.querySelector("#first-name");
+let favoriteFood = document.querySelector("#favorite-food");
+let submitButton = document.querySelector(".submit-button");
+
+submitButton.addEventListener("click", postMessage)
+
+function postMessage() {
+
+    console.log(firstName.value);
+    console.log(favoriteFood.value);
+
+    const info = {
+        firstName: firstName.value, favoriteFood: favoriteFood.value
+    };
+
+    const jsonString = JSON.stringify(info);
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(JSON.parse(xhttp.responseText));
+        }
+    }
+    xhttp.open("POST", "http://100.66.169.151:3000/chat", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+
+    xhttp.send(jsonString);
+
+}
